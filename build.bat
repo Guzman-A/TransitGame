e53e2ed3d981cbe057fd106ca5ce05a2fb8452cf
unknown <arturo.guzman.2017@gmail.com>
2019-06-02 17:15:17 -0500

    Made build file

---
+++ build.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+javac ./transitlines/TransitLine.java
+javac ./disasters/Disasters.java
+javac ./maps/Map.java
+javac ./gui/MainGameGui.java
+javac ./gui/Gui.java
+javac TransitGame.java
+java TransitGame
